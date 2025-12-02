635c65e450b2c7d8cde7d5c66dc501faa07b46c8
unilock <katherines1337@outlook.com>
2025-12-02 03:06:23 -0500

    make it actually work

--- xplat/src/main/java/cc/unilock/yeptwo/networking/payload/YepPayload.java
+++ xplat/src/main/java/cc/unilock/yeptwo/networking/payload/YepPayload.java
@@ -6,17 +6,19 @@ import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
 
+import java.nio.charset.StandardCharsets;
+
 public record YepPayload(String msg) implements CustomPacketPayload {
     public static final StreamCodec<FriendlyByteBuf, YepPayload> CODEC = CustomPacketPayload.codec(YepPayload::write, YepPayload::new);
     public static final ResourceLocation ID = ResourceLocation.fromNamespaceAndPath("yep", "generic");
     public static final Type<YepPayload> TYPE = new Type<>(ID);
 
     public YepPayload(FriendlyByteBuf buf) {
-        this(buf.readUtf());
+        this("null");
     }
 
     public void write(FriendlyByteBuf buf) {
-        buf.writeUtf(msg);
+        buf.writeBytes(msg.getBytes(StandardCharsets.UTF_8));
     }
 
     @Override
